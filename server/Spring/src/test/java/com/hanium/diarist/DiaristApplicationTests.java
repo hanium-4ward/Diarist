e5bac175ca5b142642c5f15b1f0ce1ff6bdbb16d
Jmboy713 <jimin713@naver.com>
2024-05-23 13:54:00 +0900

    feat: PROJ-54 : Spring Project 생성   - spring starter web, lombok, junit 설정

---
+++ server/Spring/src/test/java/com/hanium/diarist/DiaristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanium.diarist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaristApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
