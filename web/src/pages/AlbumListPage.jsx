701873b9483fdeb80708ddf7b76235bd5ea7b4b0
whddbsl <whddbs04140@naver.com>
2024-06-09 15:09:04 +0900

    fix : PROJ-68 : 버튼 타입 추가

--- web/src/pages/AlbumListPage.jsx
+++ web/src/pages/AlbumListPage.jsx
@@ -6,8 +6,8 @@ function AlbumListPage() {
       <h1>앨범 리스트 보기 형식 페이지</h1> {/* A11YHidden으로 가릴 예정 */}
       <h2>2024년 겨울</h2>
       <div>
-        <button>선택</button>
-        <button>최신순</button>
+        <button type='button'>선택</button>
+        <button type='button'>최신순</button>
         <img src='' alt='썸네일 보기 형식' />
         <img src='' alt='리스트 보기 형식' />
       </div>
--- web/src/pages/AlbumThumbnailPage.jsx
+++ web/src/pages/AlbumThumbnailPage.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 function AlbumThumbnailPage() {
   return (
     <div>
-          <h1>앨범 썸네일 보기 형식 페이지</h1> {/* A11YHidden으로 가릴 예정 */}
-          {/* 상단 메뉴 탭 컴포넌트로 만들어서 앨범 리스트 형식 페이지에서 재사용 예정 */}
+      <h1>앨범 썸네일 보기 형식 페이지</h1> {/* A11YHidden으로 가릴 예정 */}
+      {/* 상단 메뉴 탭 컴포넌트로 만들어서 앨범 리스트 형식 페이지에서 재사용 예정 */}
       <h2>2024년 겨울</h2>
       <div>
-        <button>선택</button>
-        <button>최신순</button>
+        <button type='button'>선택</button>
+        <button type='button'>최신순</button>
         <img src='' alt='썸네일 보기 형식' />
         <img src='' alt='리스트 보기 형식' />
       </div>
