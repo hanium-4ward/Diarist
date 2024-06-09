e4e376dc322f014272432865da658aa18535f6a5
whddbsl <whddbs04140@naver.com>
2024-06-09 14:56:36 +0900

    styling : PROJ-62 : 일기 작성 페이지 스타일링 완료

--- web/src/pages/WritingDiaryPage.jsx
+++ web/src/pages/WritingDiaryPage.jsx
@@ -2,21 +2,36 @@ import React from 'react';
 
 function WritingDiaryPage() {
   return (
-    <div className='px-[30px]'>
-      {/* 페이지 h1 a11yhidden으로 숨길 예정 */}
-      <h1>일기 작성</h1>
+    <div className='px-[4.6875%] h-screen'>
+      {/* sr-only */}
+      <h1 className='sr-only'>일기 작성</h1>
       {/* 상단 메뉴 컨테이너 */}
-      <div className='flex justify-between'>
-        <img src='/back.png' alt='뒤로가기' className='' />
-        <img src='/close.png' alt='닫기' className='' />
+      <div className='flex items-center justify-between mb-3'>
+        <img src='/btn_prev.png' alt='뒤로가기' className='w-[13.28125%]' />
+        <p className='text-[18px]'>2/3</p>
+        <img src='/btn_x.png' alt='닫기' className='w-[13.28125%]' />
       </div>
-      <h2>당신의 특별한 일상을 기록해주세요</h2>
-      <p>당신의 일기는 선택한 화가의 그림으로 재탄생 됩니다.</p>
-      <textarea className='' />
+      {/* 일기 작성 타이틀 */}
+      <div className='text-center mb-[30px]'>
+        <h2 className='text-[28px]'>당신의 특별한 일상을</h2>
+        <h2 className='text-[28px]'>기록해주세요</h2>
+        <p className='text-[16px] font-light'>
+          당신의 일기는 선택한 화가의 그림으로 재탄생 됩니다.
+        </p>
+      </div>
+      <textarea className='border border-[#666] rounded-[20px] p-[28px] w-full text-[16px] h-1/2' />
+      <p className='text-right text-[12px]'>50/1000</p>
       {/* 버튼 컨테이너 */}
-      <div className='flex justify-between'>
-        <button type='button'>건너뛰기</button>
-        <button type='button'>다 적었어요</button>
+      <div className='flex justify-between mt-[40px]'>
+        <button type='button' className='w-[47.3%] py-[18px] border border-black rounded-[15px]'>
+          건너뛰기
+        </button>
+        <button
+          type='button'
+          className='w-[47.3%] py-[18px] border bg-black text-white rounded-[15px]'
+        >
+          다 적었어요
+        </button>
       </div>
     </div>
   );
