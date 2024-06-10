98d3c6a16f8fca58a38e8818f53608ed0099389f
whddbsl <whddbs04140@naver.com>
2024-06-10 19:17:02 +0900

    fix : PROJ-72 : useEffect 제거

--- web/src/pages/SelectEmotionPage.jsx
+++ web/src/pages/SelectEmotionPage.jsx
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from 'react';
+import {React, useState} from 'react';
 import ButtonContainer from '../components/ButtonContainer';
 import Emotion from '../components/Emotion';
 import TopNavBar from '../components/TopNavBar';
