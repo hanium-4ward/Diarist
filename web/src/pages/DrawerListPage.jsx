1bb34dcfdbb659ccc0bb647cc33eb0a36be6e250
kkk1k <googija9818@gmail.com>
2024-06-11 16:49:39 +0900

    chore : PROJ-52 : 테일윈드 모듈 삭제 및 tailwindcss 관련 코드 삭제

--- web/src/pages/DrawerListPage.jsx
+++ web/src/pages/DrawerListPage.jsx
@@ -10,21 +10,21 @@ function DrawerListPage() {
     setOpenModal(false);
   };
   return (
-    <div className='relative'>
+    <div>
       {/* 페이지 h1 a11yhidden으로 숨길 예정 */}
       <h1>일기 작성</h1>
       {/* 상단 메뉴 컨테이너 */}
-      <div className='flex justify-between'>
+      <div>
         <img src='/back.png' alt='뒤로가기' className='' />
         <img src='/close.png' alt='닫기' className='' />
       </div>
-      <div className='flex justify-between'>
+      <div>
         <button type='button'>르네상스</button>
         <button type='button'>근대</button>
         <button type='button'>현대</button>
         <button type='button'>동양화</button>
       </div>
-      <div className='h-full grid grid-cols-2 place-items-center'>
+      <div>
         <figure onClick={handleModal}>
           {/* eslint-disable-next-line jsx-a11y/alt-text */}
           <img src='/drawer.jpg' />
