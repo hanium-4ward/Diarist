e821cfadf9ab62a8664c5f817757d83610469078
kkk1k <googija9818@gmail.com>
2024-06-12 12:17:16 +0900

    chore : PROJ-52 : 웹 폰트로 변경

--- web/src/GlobalStyle.jsx
+++ web/src/GlobalStyle.jsx
@@ -1,81 +1,15 @@
-// GlobalStyle.js
 import {createGlobalStyle} from 'styled-components';
-import './assets/fonts/Pretendard-Black.otf';
-import './assets/fonts/Pretendard-Bold.otf';
-import './assets/fonts/Pretendard-ExtraBold.otf';
-import './assets/fonts/Pretendard-ExtraLight.otf';
-import './assets/fonts/Pretendard-Light.otf';
-import './assets/fonts/Pretendard-Medium.otf';
-import './assets/fonts/Pretendard-Regular.otf';
-import './assets/fonts/Pretendard-SemiBold.otf';
-import './assets/fonts/Pretendard-Thin.otf';
 
 const GlobalStyle = createGlobalStyle`
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-Black.otf') format('opentype');
-        font-weight: 900;
-        font-style: normal;
-    }
 
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-Bold.otf') format('opentype');
-        font-weight: 700;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-ExtraBold.otf') format('opentype');
-        font-weight: 800;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-ExtraLight.otf') format('opentype');
-        font-weight: 200;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-Light.otf') format('opentype');
-        font-weight: 300;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-Medium.otf') format('opentype');
-        font-weight: 500;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-Regular.otf') format('opentype');
-        font-weight: 400;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-SemiBold.otf') format('opentype');
-        font-weight: 600;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'Pretendard';
-        src: url('../assets/font/Pretendard-Thin.otf') format('opentype');
-        font-weight: 100;
-        font-style: normal;
-    }
-
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
     html, body, * {
-        font-family: 'Pretendard', sans-serif;
+        font-family: 'Pretendard-Regular', sans-serif;
     }
 
     body {
