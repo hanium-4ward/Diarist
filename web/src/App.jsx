c416ccc1dba1e3729f529627f4739de99e4e7d9a
whddbsl <whddbs04140@naver.com>
2024-06-12 16:36:37 +0900

    styling : PROJ-16 : ThemeProvider 설정

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Routes, Route} from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
 import Calendar from './pages/Calendar';
 import WritingDiaryPage from './pages/WritingDiaryPage';
 import DrawCompletedPage from './pages/DrawCompletedPage';
@@ -7,12 +8,33 @@ import SelectDrawerPage from './pages/SelectDrawerPage';
 import DrawerListPage from './pages/DrawerListPage';
 import AlbumThumbnailPage from './pages/AlbumThumbnailPage';
 import AlbumListPage from './pages/AlbumListPage';
-import SelectEmotionPage from './pages/SelectEmotionPage';
 import GlobalStyle from './GlobalStyle';
 
+const FIGMA_WIDTH = 640;
+
 function App() {
+  const [widthRatio, setWidthRatio] = useState(1);
+
+  useEffect(() => {
+    const updateWidthRatio = () => {
+      const newWidthRatio = window.innerWidth / FIGMA_WIDTH;
+      setWidthRatio(newWidthRatio);
+    };
+
+    updateWidthRatio();
+    window.addEventListener('resize', updateWidthRatio);
+
+    return () => {
+      window.removeEventListener('resize', updateWidthRatio);
+    };
+  }, []);
+
+  const theme = {
+    widthRatio,
+  };
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Routes>
         <Route path='/' element={<Calendar />} />
@@ -23,9 +45,9 @@ function App() {
         <Route path='write' element={<WritingDiaryPage />} />
         <Route path='thumbnail' element={<AlbumThumbnailPage />} />
         <Route path='list' element={<AlbumListPage />} />
-        <Route path='emotion' element={<SelectEmotionPage />} />
       </Routes>
-    </>
+    </ThemeProvider>
   );
 }
+
 export default App;
