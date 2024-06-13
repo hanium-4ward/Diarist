6d926f66e5207a43145942d6fb75137e9879b707
Jmboy713 <jimin713@naver.com>
2024-06-13 19:56:01 +0900

    refactor : PROJ-115 : swagger api response 설명 어노테이션 추가

--- server/Spring/src/main/java/com/hanium/diarist/domain/diary/controller/DiaryController.java
+++ server/Spring/src/main/java/com/hanium/diarist/domain/diary/controller/DiaryController.java
@@ -1,20 +1,23 @@
 package com.hanium.diarist.domain.diary.controller;
 
+import com.hanium.diarist.common.response.ErrorResponse;
 import com.hanium.diarist.common.response.SuccessResponse;
-import com.hanium.diarist.domain.diary.dto.AdResponse;
-import com.hanium.diarist.domain.diary.dto.BookmarkDiaryRequest;
-import com.hanium.diarist.domain.diary.dto.BookmarkDiaryResponse;
-import com.hanium.diarist.domain.diary.dto.CreateDiaryRequest;
+import com.hanium.diarist.domain.diary.domain.Diary;
+import com.hanium.diarist.domain.diary.dto.*;
 import com.hanium.diarist.domain.diary.service.CreateDiaryConsumerService;
 import com.hanium.diarist.domain.diary.service.CreateDiaryProducerService;
 import com.hanium.diarist.domain.diary.service.DiaryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.connector.Response;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +26,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
-import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/api/v1/diary")
@@ -36,24 +38,28 @@ public class DiaryController {
     private final CreateDiaryConsumerService createDiaryConsumerService;
     private final DiaryService diaryService;
 
-    @PostMapping("/create")
-    @Operation(summary = "일기 생성 요청", description = "일기 생성 요청 API.")
+    @PostMapping("create/no_ad")
+    @Operation(summary = "일기 생성 요청, 광고 없음", description = "일기 생성 요청 API.")
     @ApiResponse(responseCode = "200", description = "일기 생성 요청 메세지큐에 등록 완료")
-    @ApiResponse(responseCode = "400", description = "잘못된 요청")
-    public ResponseEntity<String> createDiary(@Valid @RequestBody CreateDiaryRequest createDiaryRequest) {
+    public ResponseEntity<SuccessResponse<String>> createDiary(@Valid @RequestBody CreateDiaryRequest createDiaryRequest) {
         createDiaryProducerService.sendCreateDiaryMessage(createDiaryRequest);
-        return new ResponseEntity<>("일기 생성 요청 메세지큐에 등록 완료", HttpStatus.OK);
+        return SuccessResponse.of("일기 생성 요청 메세지큐에 등록 완료").asHttp(HttpStatus.CREATED);// 광고 시청 여부 반환
     }
 
-    @PostMapping
-    @Operation(summary = "광고 시청", description = "광고 시청 API.")
-    public SuccessResponse<AdResponse> createDiaryWithAd(@Valid @RequestBody CreateDiaryRequest createDiaryRequest) {
+    @PostMapping("create/ad")
+    @Operation(summary = "일기 생성 요청 및 광고 시청", description = "일기 생성 요청 및 광고 시청 API.")
+    @ApiResponse(responseCode = "200", description = "일기 생성 요청 메세지큐에 등록 완료")
+    public ResponseEntity<SuccessResponse<AdResponse>> createDiaryWithAd(@Valid @RequestBody CreateDiaryRequest createDiaryRequest) {
         boolean adRequired = createDiaryProducerService.sendCreateDiaryMessageWithAd(createDiaryRequest);
-        return SuccessResponse.of(new AdResponse(adRequired));// 광고 시청 여부 반환
+        return SuccessResponse.of(new AdResponse(adRequired)).asHttp(HttpStatus.CREATED);// 광고 시청 여부 반환
     }
+
+
 
 
     @GetMapping(value = "/kafka-response", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @Operation(summary = "일기 생성 요청 응답", description = "일기 생성 완료 요청을 클라이언트에 던져주는 API.")
+    @ApiResponse(responseCode = "200", description = "일기 생성 완료 요청을 클라이언트에 던져주는 API", content = @Content(array = @ArraySchema(schema = @Schema(implementation = CreateDiaryResponse.class))))
     public SseEmitter kafkaResponse() {
         // kafka consumer 가 emitter 에 데이터를 보내면 emitter 가 클라이언트에게 데이터를 보냄
         return createDiaryConsumerService.addEmitter();
@@ -61,6 +67,7 @@ public class DiaryController {
 
     @PostMapping("/bookmark")
     @Operation(summary = "일기 즐겨찾기 (1개)", description = "일기 즐겨찾기 API.")
+    @ApiResponse(responseCode = "200", description = "일기 즐겨찾기 성공")
     public ResponseEntity<SuccessResponse<BookmarkDiaryResponse>> bookmarkDiary(@RequestBody BookmarkDiaryRequest request) {
         BookmarkDiaryResponse response = diaryService.bookmarkDiary(request.getDiaryId(), request.isFavorite());
         return SuccessResponse.of(response).asHttp(HttpStatus.OK);
@@ -68,6 +75,7 @@ public class DiaryController {
 
     @PostMapping("/bookmark/delete")
     @Operation(summary = "일기 즐겨찾기 해제", description = "앨범페이지 일기 즐겨찾기 해제 API")
+    @ApiResponse(responseCode = "200",description = "일기 즐겨찾기 해제 성공")
     public ResponseEntity<SuccessResponse<List<BookmarkDiaryResponse>>> deleteBookmarkDiary(@RequestBody List<Long> diaryIdList) {
         List<BookmarkDiaryResponse> bookmarkDiaryResponses = diaryService.deleteBookmarkDiary(diaryIdList);
         return SuccessResponse.of(bookmarkDiaryResponses).asHttp(HttpStatus.OK);
