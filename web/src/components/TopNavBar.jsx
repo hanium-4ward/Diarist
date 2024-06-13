747bc3d39826969d54baac735d502123cdd22377
whddbsl <whddbs04140@naver.com>
2024-06-13 12:39:51 +0900

    styling : PROJ-62 : 상단 네비바 이미지 파일 변경 및 스타일링 완료

--- web/src/components/TopNavBar.jsx
+++ web/src/components/TopNavBar.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: ${props => 100 * props.theme.widthRatio}px;
+`;
+
+const Span = styled.span`
+  text-align: center;
+  font-size: ${props => 28 * props.theme.widthRatio}px;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.42px;
+`;
+
+const Img = styled.img`
+  height: ${props => 33 * props.theme.widthRatio}px;
+  padding-left: ${props => 34 * props.theme.widthRatio}px;
+  padding-top: ${props => 33.5 * props.theme.widthRatio}px;
+  padding-right: ${props => 34 * props.theme.widthRatio}px;
+  padding-bottom: ${props => 33.5 * props.theme.widthRatio}px;
+`;
+
+const CloseImg = styled.img`
+  height: ${props => 33 * props.theme.widthRatio}px;
+  padding-left: ${props => 34 * props.theme.widthRatio}px;
+  padding-top: ${props => 33.5 * props.theme.widthRatio}px;
+  padding-right: ${props => 34 * props.theme.widthRatio}px;
+  padding-bottom: ${props => 33.5 * props.theme.widthRatio}px;
+`;
 function TopNavBar({progress}) {
   return (
-    <div>
-      <img src='/btn_prev.png' alt='뒤로가기' />
-      <span>
+    <Header>
+      <Img src='/btn_prev.png' alt='뒤로가기' />
+      <Span>
         <span>{progress}</span> / 3
-      </span>
-      <img src='/btn_x.png' alt='닫기' />
-    </div>
+      </Span>
+      <CloseImg src='/btn_x.png' alt='닫기' />
+    </Header>
   );
 }
 
