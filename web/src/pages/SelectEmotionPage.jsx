3a66fd49e71b7b7f864969b87552f9438ac0cf9c
whddbsl <whddbs04140@naver.com>
2024-06-16 20:48:22 +0900

    markup : PROJ-72 : 서브 타이틀 제거

--- web/src/pages/SelectEmotionPage.jsx
+++ web/src/pages/SelectEmotionPage.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import styled, {useTheme} from 'styled-components';
+import styled from 'styled-components';
 import ButtonContainer from '../components/ButtonContainer';
 import Emotion from '../components/Emotion';
 import TopNavBar from '../components/TopNavBar';
@@ -24,27 +24,24 @@ const H2 = styled.h2`
   letter-spacing: -0.63px;
 `;
 
-const P = styled.p`
-  text-align: center;
-  font-size: ${props => 24 * props.theme.widthRatio}px;
-  font-weight: 300;
-  line-height: normal;
-  letter-spacing: -0.36px;
-  margin-top: ${props => 20 * props.theme.widthRatio}px;
-`;
-
 const EmotionContainer = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  gap: ${props => 60 * props.theme.widthRatio}px;
+  gap: ${props => 40 * props.theme.widthRatio}px;
+  justify-content: space-between;
   margin-top: ${props => 40 * props.theme.widthRatio}px;
   margin-left: ${props => 80 * props.theme.widthRatio}px;
   margin-right: ${props => 80 * props.theme.widthRatio}px;
 `;
 
-function SelectEmotionPage() {
-  const theme = useTheme();
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+`;
 
+function SelectEmotionPage() {
   const [selectedEmotion, setSelectedEmotion] = useState('0');
 
   const handleEmotionClick = id => {
@@ -52,38 +49,37 @@ function SelectEmotionPage() {
   };
 
   return (
-    <>
-      <A11yHidden>감정 선택 페이지</A11yHidden>
-      <TopNavBar progress={1} />
-
+    <Container>
       <div>
-        <H2>당신의 하루는 어떤</H2>
+        <A11yHidden>감정 선택 페이지</A11yHidden>
+        <TopNavBar progress={1} />
+        {/* 캘린더 페이지 완성되면 날짜부분 함수 들어갈 예정 */}
+        <H2>6월 16일 하루는 어떤</H2>
         <H2>감정이였나요?</H2>
-        <P>가장 많이 느낀 감정을 찾아보세요</P>
-
-        <EmotionContainer>
-          <Emotion src='/emotion.png' label='행복' onClick={() => handleEmotionClick(1)} />
-          <Emotion src='/emotion.png' label='기쁨' onClick={() => handleEmotionClick(2)} />
-          <Emotion src='/emotion.png' label='감사' onClick={() => handleEmotionClick(3)} />
-          <Emotion src='/emotion.png' label='기대' onClick={() => handleEmotionClick(4)} />
-          <Emotion src='/emotion.png' label='신남' onClick={() => handleEmotionClick(5)} />
-          <Emotion src='/emotion.png' label='설렘' onClick={() => handleEmotionClick(6)} />
-          <Emotion src='/emotion.png' label='슬픔' onClick={() => handleEmotionClick(7)} />
-          <Emotion src='/emotion.png' label='화남' onClick={() => handleEmotionClick(8)} />
-          <Emotion src='/emotion.png' label='짜증' onClick={() => handleEmotionClick(9)} />
-          <Emotion src='/emotion.png' label='걱정' onClick={() => handleEmotionClick(10)} />
-          <Emotion src='/emotion.png' label='후회' onClick={() => handleEmotionClick(11)} />
-          <Emotion src='/emotion.png' label='피곤' onClick={() => handleEmotionClick(12)} />
-        </EmotionContainer>
-
-        <ButtonContainer
-          firstLabel='건너뛰기'
-          secondLabel='선택완료'
-          nextPath='write'
-          emotionId={selectedEmotion}
-        />
       </div>
-    </>
+
+      <EmotionContainer>
+        <Emotion src='/emotion.png' label='행복' onClick={() => handleEmotionClick(1)} />
+        <Emotion src='/emotion.png' label='기쁨' onClick={() => handleEmotionClick(2)} />
+        <Emotion src='/emotion.png' label='감사' onClick={() => handleEmotionClick(3)} />
+        <Emotion src='/emotion.png' label='기대' onClick={() => handleEmotionClick(4)} />
+        <Emotion src='/emotion.png' label='신남' onClick={() => handleEmotionClick(5)} />
+        <Emotion src='/emotion.png' label='설렘' onClick={() => handleEmotionClick(6)} />
+        <Emotion src='/emotion.png' label='슬픔' onClick={() => handleEmotionClick(7)} />
+        <Emotion src='/emotion.png' label='화남' onClick={() => handleEmotionClick(8)} />
+        <Emotion src='/emotion.png' label='짜증' onClick={() => handleEmotionClick(9)} />
+        <Emotion src='/emotion.png' label='걱정' onClick={() => handleEmotionClick(10)} />
+        <Emotion src='/emotion.png' label='후회' onClick={() => handleEmotionClick(11)} />
+        <Emotion src='/emotion.png' label='피곤' onClick={() => handleEmotionClick(12)} />
+      </EmotionContainer>
+
+      <ButtonContainer
+        firstLabel='건너뛰기'
+        secondLabel='선택완료'
+        nextPath='write'
+        emotionId={selectedEmotion}
+      />
+    </Container>
   );
 }
 
