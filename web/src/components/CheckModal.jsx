6778516aa12c05c3a0fcd1e012f81d554678482e
kkk1k <googija9818@gmail.com>
2024-06-19 17:10:10 +0900

    feat : PROJ-82 : 닫기 버튼 클릭시 alert 생성 및 기능 추가

--- web/src/components/CheckModal.jsx
+++ web/src/components/CheckModal.jsx
@@ -12,15 +12,6 @@ const slideUp = keyframes`
   }
 `;
 
-const slideDown = keyframes`
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(100%);
-  }
-`;
-
 const ModalBackground = styled.section`
   position: fixed;
   left: 0;
@@ -28,125 +19,112 @@ const ModalBackground = styled.section`
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ModalContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  bottom: 0;
-  width: 100%;
+  width: 80%;
+  height: auto;
   background: white; /* 컨테이너의 배경색 설정 */
-  border-radius: 30px 30px 0 0; /* 모서리 둥글게 설정 */
+  border-radius: ${props => 30 * props.theme.widthRatio}px;
   padding-top: ${props => 16 * props.theme.widthRatio}px;
-  animation: ${props => (props.isClosing ? slideDown : slideUp)} 0.7s ease;
+  padding-left: ${props => 20 * props.theme.widthRatio}px;
+  padding-right: ${props => 20 * props.theme.widthRatio}px;
+  animation: ${slideUp} 0.7s ease;
 `;
 
-const H3Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const H3 = styled.h3`
-  padding-top: ${props => 66 * props.theme.widthRatio}px;
+const Title = styled.p`
   color: #000;
   text-align: center;
-  font-size: ${props => 40 * props.theme.widthRatio}px;
-  font-weight: 400;
-  line-height: normal;
-  letter-spacing: ${props => -0.6 * props.theme.widthRatio}px;
-`;
-
-const ExampleImg = styled.img`
-  margin-top: ${props => 40 * props.theme.widthRatio}px;
-  border-radius: 20px;
-  width: ${props => 300 * props.theme.widthRatio}px;
-  margin-bottom: ${props => 40 * props.theme.widthRatio}px;
-`;
-
-const P = styled.p`
-  color: #000;
-  text-align: center;
-  font-size: ${props => 24 * props.theme.widthRatio}px;
-  font-weight: 300;
+  font-size: ${props => 30 * props.theme.widthRatio}px;
+  font-weight: 700;
   line-height: normal;
   letter-spacing: ${props => -0.36 * props.theme.widthRatio}px;
   word-break: keep-all;
+  margin-top: ${props => 20 * props.theme.widthRatio}px;
+`;
+
+const SubTitle = styled.p`
+  color: #585859;
+  text-align: center;
+  font-size: ${props => 26 * props.theme.widthRatio}px;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: ${props => -0.36 * props.theme.widthRatio}px;
+  word-break: keep-all;
+  margin-top: ${props => 25 * props.theme.widthRatio}px;
 `;
 
 const Button = styled.button`
   margin-top: ${props => 40 * props.theme.widthRatio}px;
   margin-bottom: ${props => 40 * props.theme.widthRatio}px;
   border-radius: ${props => 15 * props.theme.widthRatio}px;
-  background: #000;
-  width: ${props => 580 * props.theme.widthRatio}px;
+  background: ${props => (props.$isCancel ? '#CFCFCF' : '#000')};
+  width: 48%;
   height: ${props => 80 * props.theme.widthRatio}px;
-  color: #fff;
+  color: ${props => (props.$isCancel ? '#000' : '#fff')};
   text-align: center;
   font-size: ${props => 24 * props.theme.widthRatio}px;
   font-weight: 500;
   line-height: normal;
 `;
 
-function CheckModal({openModal, closeModal, data}) {
-  const {sheet} = useBottomSheet();
-  if (!openModal) return null;
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-  const [isClosing, setIsClosing] = useState(false);
-  const startYRef = useRef(0);
-  const currentYRef = useRef(0);
+const SVGWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: ${props => 0 * props.theme.widthRatio}px;
+`;
 
-  useEffect(() => {
-    if (openModal) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'unset';
-    }
-    return () => {
-      document.body.style.overflow = 'unset';
-    };
-  }, [openModal]);
+function CheckModal({isOpen, closeModal}) {
+  if (!isOpen) return null;
 
-  const handleTouchStart = e => {
-    startYRef.current = e.touches[0].clientY;
-  };
-
-  const handleTouchMove = e => {
-    currentYRef.current = e.touches[0].clientY;
-  };
-
-  const handleTouchEnd = () => {
-    if (startYRef.current < currentYRef.current && currentYRef.current - startYRef.current > 100) {
-      setIsClosing(true);
-      setTimeout(() => {
-        closeModal();
-        setIsClosing(false);
-      }, 700);
-    }
+  const handleClose = () => {
+    (window.ReactNativeWebView || window).postMessage('closeWebView');
   };
 
   return ReactDOM.createPortal(
-    <ModalBackground>
+    <ModalBackground onClick={closeModal}>
       <ModalContainer
-        isClosing={isClosing}
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
+        onClick={e => {
+          e.stopPropagation();
+        }}
       >
-        <Wrapper>
-          <Handle />
-        </Wrapper>
-        <H3Wrapper>
-          <H3>{data.artistName}</H3>
-          {/* <CloseButton src='/btn_x.png' onClick={closeModal} /> */}
-        </H3Wrapper>
-        <ExampleImg src={data.artistPicture} />
-        <P>당신이 일기는 위의 그림 풍으로 재탄생됩니다.</P>
-        <P>이 화가와의 작업을 원하시면 선택완료 버튼을 눌러주세요</P>
-        <Button>선택완료</Button>
+        <SVGWrapper>
+          <svg
+            fill='#CFCFCF'
+            viewBox='-1.7 0 20.4 20.4'
+            xmlns='http://www.w3.org/2000/svg'
+            className='cf-icon-svg'
+            width='100'
+            height='100'
+          >
+            <g id='SVGRepo_bgCarrier' strokeWidth='0' />
+            <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' />
+            <g id='SVGRepo_iconCarrier'>
+              <path d='M16.406 10.283a7.917 7.917 0 1 1-7.917-7.917 7.916 7.916 0 0 1 7.917 7.917zM9.48 14.367a1.003 1.003 0 1 0-1.004 1.003 1.003 1.003 0 0 0 1.004-1.003zM7.697 11.53a.792.792 0 0 0 1.583 0V5.262a.792.792 0 0 0-1.583 0z' />
+            </g>
+          </svg>
+        </SVGWrapper>
+
+        <Title>일기 작성을 그만두시나요?</Title>
+        <SubTitle>지금 작성한 내용들이 모두 사라집니다.</SubTitle>
+        <ButtonWrapper>
+          {' '}
+          <Button onClick={closeModal} $isCancel>
+            취소
+          </Button>
+          <Button onClick={handleClose}>나가기</Button>
+        </ButtonWrapper>
       </ModalContainer>
     </ModalBackground>,
     document.getElementById('modal-root'),
@@ -154,7 +132,3 @@ function CheckModal({openModal, closeModal, data}) {
 }
 
 export default CheckModal;
-
-export const MIN_Y = 60; // 바텀시트가 최대로 높이 올라갔을 때의 y 값
-export const MAX_Y = window.innerHeight - 80; // 바텀시트가 최소로 내려갔을 때의 y 값
-export const BOTTOM_SHEET_HEIGHT = window.innerHeight - MIN_Y; // 바텀시트의 세로 길이
