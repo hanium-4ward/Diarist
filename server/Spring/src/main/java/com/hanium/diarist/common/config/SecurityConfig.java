400a56ff38bfea4e7406a39fe769df8f752093d8
Jmboy713 <jimin713@naver.com>
2024-07-01 11:24:40 +0900

    feat : PROJ-75 : spring security의 filter chain 적용 - jwt 가 있어야 api 접근 가능

---
+++ server/Spring/src/main/java/com/hanium/diarist/common/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.hanium.diarist.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hanium.diarist.common.security.jwt.JwtAuthenticationFilter;
+import com.hanium.diarist.common.security.jwt.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(permitAllEndpointList()).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider, permitAllEndpointList());
+    }
+
+    private String[] permitAllEndpointList() {
+        return new String[]{
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/oauth2/kakao/**"
+
+        };
+    }
+}
